e99210bcc3f23e22a9a6d0b83990bd4f2d04bb08
vietman2 <vietman0@gmail.com>
2023-04-16 16:22:27 +0900

    fix teamrecord

--- backend/team/models.py
+++ backend/team/models.py
@@ -15,7 +15,7 @@ class Team_Record(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=10, null=True)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, default=None, related_name='group')
     second_round = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, default=None, related_name='second_round')
     captain_Name = models.CharField(max_length=10)
     captain_PhoneNumber = models.CharField(max_length=15)
