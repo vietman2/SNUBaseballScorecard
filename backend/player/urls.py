a6b0db3479b5d1c1f7def3aebc260d838ae0326e
vietman2 <vietman0@gmail.com>
2023-05-01 18:50:44 +0900

    add search and create player feature

--- backend/player/urls.py
+++ backend/player/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    ## NEEDFIX: This is a stub view. It should be replaced with a real view.
-    path('', views.index, name='index'),
+    path('search_by_name/<str:name>/', views.search_by_name, name='search_by_name'),
+    path('search_by_student_id/<str:student_id>/', views.search_by_student_id, name='search_by_student_id'),
+    path('create/<str:student_id>/<str:name>/<str:elite>/<str:team_name>/<str:college>/<str:department>/<str:status>/', views.create, name='create'),
+    path('create_tournament/<str:student_id>/<str:team_name>/<str:college>/<str:department>/<str:status>/', views.create_tournament, name='create_tournament'),
 ]
--- backend/player/views.py
+++ backend/player/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+
+from .models import Player, Player_Tournament
+from team.models import Team
 
 # Create your views here.
 
-## NEEDFIX: This is a stub view. It should be replaced with a real view.
-def index(request):
-    return render(request, 'player/index.html')
+def search_by_name(request, name):
+    if request.method == 'GET':
+        players = Player.objects.filter(name=name).values()
+        player_list = [p for p in players]
+        return JsonResponse(player_list, safe=False)
+    
+def search_by_student_id(request, student_id):
+    if request.method == 'GET':
+        players = Player.objects.filter(student_id=student_id).values()
+        player_list = [p for p in players]
+        return JsonResponse(player_list, safe=False)
+    
+def create(request, student_id, name, elite, team_name, college, department, status):
+    if request.method == 'POST':
+        player = Player(student_id=student_id, name=name, elite=elite)
+        player.save()
+        team = Team.objects.get(name=team_name)
+        player_tournament = Player_Tournament(player=player, team=team_name, college=college, department=department, status=status)
+        player_tournament.save()
+        return JsonResponse({'result': 'success'})
+    
+def create_tournament(request, student_id, team_name, college, department, status):
+    if request.method == 'POST':
+        player = Player.objects.get(student_id=student_id)
+        team = Team.objects.get(name=team_name)
+        player_tournament = Player_Tournament(player=player, team=team_name, college=college, department=department, status=status)
+        player_tournament.save()
+        return JsonResponse({'result': 'success'})
