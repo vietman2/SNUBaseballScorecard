93f17a25881217aa8d5c96c7549ec677aa94d11c
vietman2 <vietman0@gmail.com>
2023-05-01 18:50:22 +0900

    fix game model and view temp.

--- backend/game/models.py
+++ backend/game/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from team.models import Team
 from tournament.models import Tournament
+from group.models import Group
 
 # Create your models here.
 
@@ -14,4 +15,11 @@ class Game(models.Model):
     team1 = models.ForeignKey(Team, on_delete=models.DO_NOTHING, related_name='team1')
     team2 = models.ForeignKey(Team, on_delete=models.DO_NOTHING, related_name='team2')
     done = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.gameName
+    
+class GroupGame(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
     
