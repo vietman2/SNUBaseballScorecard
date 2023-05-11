a9faf8504d44bc3adbf7cc873a21511b9ed7b66c
vietman2 <vietman0@gmail.com>
2023-05-11 12:21:15 +0900

    fix referee model: link with user

--- backend/referee/models.py
+++ backend/referee/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
 from tournament.models import Tournament
+from account.models import CustomUser
 
 # Create your models here.
 
 class Referee(models.Model):
-    name = models.CharField(max_length=10)
+    referee = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
 
     class Meta:
-        unique_together = ('name', 'tournament')
+        unique_together = ('referee', 'tournament')
 
     def __str__(self):
-        return self.name
+        return self.referee.name + ' ' + self.tournament.name
     
