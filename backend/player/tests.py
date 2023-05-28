71ccc6252400dae7a6697367d9fe51f3c72f943a
vietman2 <vietman0@gmail.com>
2023-05-28 23:02:52 +0900

    excel_file_API

--- backend/player/tests.py
+++ backend/player/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Team, Team_Record
+from tournament.models import Tournament
+from group.models import Group
+
+class PlayerTestCase(TestCase):
+    def setUp(self):
+        pass
--- backend/player/views.py
+++ backend/player/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import MultiPartParser
 
-from .models import Player, Player_Tournament
+from .models import Player
 
 # Create your views here.
 
@@ -20,14 +20,14 @@ def excel_file(request):
     df = pd.read_excel(excel_file, engine='openpyxl')
 
     summary = {
-        'new_players': 0,
-        'updated_players': 0,
+        'new_players': [],
+        'similar_players': [],
+        'existing_players': [],
         'errors': [],
     }
 
     for i in range(0, df.shape[0]):
         row = df.iloc[i]
-        idx = df.index[i]
 
         college = row['대 학']
         department = row['학 과']
@@ -35,7 +35,41 @@ def excel_file(request):
         name = row['성 명']
         status = row['재적현황']
 
-        print("대학: " + college + ", 학과: " + department + ", 학번: " + str(student_id) + ", 이름: " + name + ", 재적현황: " + status)
+        info = {
+            'college': college,
+            'department': department,
+            'student_id': student_id,
+            'name': name,
+            'status': status,
+        }
+
+        player_exists = Player.objects.filter(student_id=student_id).exists()
+
+        if player_exists is False:
+            ## 같은 학번이 없음
+            players_with_same_name = Player.objects.filter(name=name).exists()
+
+            if players_with_same_name is False:
+                ## 같은 이름이 없음
+                summary['new_players'].append(info)
+            else:
+                ## 같은 이름이 있음
+                data = {
+                    'received': info,
+                    'existing': [],
+                }
+                data['existing'] = players_with_same_name
+                summary['similar_players'].append(info)
+
+        else:
+            ## 같은 학번이 있음
+            player = Player.objects.get(student_id=student_id)
+            if player.name == name:
+                ## 이름이 같음
+                summary['existing_players'].append(info)
+            else:
+                ## 이름이 다름
+                summary['errors'].append(info)
 
 
 
