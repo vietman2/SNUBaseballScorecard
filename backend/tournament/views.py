c76120da6cc198a1f7e0166690933cbcae83f31b
vietman2 <vietman0@gmail.com>
2023-06-23 13:09:29 +0900

    finish tournament-apis

--- backend/tournament/views.py
+++ backend/tournament/views.py
@@ -1,9 +1,13 @@
-from django.http import JsonResponse
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 
 from .models import Tournament
-from .serializers import TournamentSerializer
+from .serializers import SimpleTournamentSerializer, DetailedTournamentSerializer
 
 class TournamentsAPI(ListCreateAPIView):
+    queryset = Tournament.objects.all().order_by('-start_date')
+    serializer_class = SimpleTournamentSerializer
+
+class TournamentAPI(RetrieveUpdateAPIView):
     queryset = Tournament.objects.all()
-    serializer_class = TournamentSerializer
+    serializer_class = DetailedTournamentSerializer
+    lookup_field = getattr(Tournament, 'name')
