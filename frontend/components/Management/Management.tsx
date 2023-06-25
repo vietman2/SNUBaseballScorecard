ce343ac66cfbdb386cca0c1ba80ac57ecce48ceb
vietman2 <vietman0@gmail.com>
2023-06-25 11:00:02 +0900

    add features

--- frontend/components/Management/Management.tsx
+++ frontend/components/Management/Management.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View } from "react-native";
+import { List } from "react-native-paper";
 import { StackNavigationProp } from "@react-navigation/stack";
 
 import { RootStackParamList } from "../../App";
 import { styles } from "./styles";
+import { fetchTournaments } from "../../services/tournament";
+import { fetchTeams } from "../../services/team";
+import { TeamInfoType, TournamentType } from "../../variables/types";
+import RegistrationTable from "../../containers/Tables/RegistrationTable";
 
-type ManagementNavigationProp = StackNavigationProp<
+export type ManagementNavigationProp = StackNavigationProp<
   RootStackParamList,
   "Management"
 >;
@@ -15,12 +20,61 @@ interface Props {
 }
 
 export default function Management({ navigation }: Props) {
+  const [expanded, setExpanded] = useState<boolean>(false);
+  const [tournamentList, setTournamentList] = useState<TournamentType[]>([]);
+  const [selectedTournament, setSelectedTournament] = useState<string>("대회 선택");
+  const [teams, setTeams] = useState<TeamInfoType[]>([]);
+
+  const handlePress = () => setExpanded(!expanded);
+  const handleItemPress = (item: string) => {
+    setSelectedTournament(item);
+    setExpanded(false);
+  };
+
+  const renderTournaments = async () => {
+    const tournaments: TournamentType[] = await fetchTournaments();
+
+    setTournamentList(tournaments);
+  };
+
+  const getTeams = async () => {
+    if (selectedTournament === "대회 선택") return;
+    const teams: TeamInfoType[] = await fetchTeams(selectedTournament);
+
+    setTeams(teams);
+  }
+
+  useEffect(() => {
+    renderTournaments();
+  }, []);
+
+  useEffect(() => {
+    getTeams();
+  }, [selectedTournament]);
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.title}>대회 관리</Text>
-      </View>
+      <Text style={styles.title}>대회 관리</Text>
+      <List.Section>
+        <List.Accordion
+          title={selectedTournament}
+          expanded={expanded}
+          onPress={handlePress}
+          style={{ backgroundColor: "#fff" }}
+          titleStyle={{ fontSize: 18, fontWeight: "bold", textAlign: "center" }}
+        >
+          {tournamentList.map((tournament) => (
+            <List.Item
+              key={tournament.name}
+              title={tournament.name}
+              onPress={() => handleItemPress(tournament.name)}
+              titleStyle={{ fontSize: 16, textAlign: "center" }}
+            />
+          ))}
+        </List.Accordion>
+      </List.Section>
+      <View style={{ flex: 1 }}>{selectedTournament === "대회 선택" ? <></> : <RegistrationTable tournament={selectedTournament} teams={teams} navigation={navigation} />}</View>
+      
     </View>
   );
 }
--- frontend/components/Management/styles.ts
+++ frontend/components/Management/styles.ts
@@ -11,46 +11,10 @@ export const styles = StyleSheet.create({
     textAlign: "center",
     margin: 10,
   },
-  pickerContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-  },
   subtitle: {
     fontSize: 20,
     fontWeight: "bold",
     textAlign: "center",
     margin: 10,
-  },
-  text: {
-    fontSize: 16,
-    textAlign: "center",
-    margin: 10,
-    padding: 10,
-  },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10,
-    margin: 10,
-    borderRadius: 5,
-  },
-  buttonText: {
-    fontSize: 16,
-    color: "#000000",
-    textAlign: "center",
-  },
-});
-
-export const dropdownStyles = StyleSheet.create({
-  dropdownLabel: {
-    fontSize: 18,
-    textAlign: "center",
-    fontWeight: "bold",
-  },
-  dropdownText: {
-    fontSize: 18,
-    textAlign: "center",
   },
 });
