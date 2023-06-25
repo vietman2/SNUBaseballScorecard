48107d3f31b66ccee0d9456542df19e3fc0d2af5
vietman2 <vietman0@gmail.com>
2023-06-25 11:00:23 +0900

    fix

--- frontend/services/team.ts
+++ frontend/services/team.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { API_BASE_URL } from "./apiConfig";
 
-/*
+
 export async function fetchTeams(tournament: string) {
   const url = `${API_BASE_URL}/api/team/records/`;
   const response = await axios.get(url, {
@@ -12,7 +12,7 @@ export async function fetchTeams(tournament: string) {
   });
   return response.data;
 }
-
+/*
 export async function fetchPlayersTeam(tournament: string, team: string) {
   const url = `${API_BASE_URL}/api/team/records/`;
   const response = await axios.get(url, {
